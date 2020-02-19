2a95a01adf937c3f09c595f4913b7f8f55189320
Abhishek Talekar <talekarabhishek02@gmail.com>
2020-02-19 23:13:26 +0530

    Entity classes

---
+++ HibernateGetVsLoad/src/com/gvl/entities/Person.java
@@ -0,0 +1,49 @@
+package com.gvl.entities;
+
+import java.io.Serializable;
+
+public class Person implements Serializable{
+	protected int personId;
+	protected String personName;
+	protected String personSurname;
+	protected String emailAddress;
+	protected int age;
+	protected String mobileNumber;
+	public int getPersonId() {
+		return personId;
+	}
+	public void setPersonId(int personId) {
+		this.personId = personId;
+	}
+	public String getPersonName() {
+		return personName;
+	}
+	public void setPersonName(String personName) {
+		this.personName = personName;
+	}
+	public String getPersonSurname() {
+		return personSurname;
+	}
+	public void setPersonSurname(String personSurname) {
+		this.personSurname = personSurname;
+	}
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+	
+}
